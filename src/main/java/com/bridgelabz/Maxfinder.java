b76259db10088b6d1b60ab61b021e841adc9a770
aishwaryap14 <aishwarya.potdar123@gamil.com>
2021-04-07 20:01:21 +0530

    Refactored: function to find max value using generics

--- src/main/java/com/bridgelabz/Maxfinder.java
+++ src/main/java/com/bridgelabz/Maxfinder.java
@@ -1,8 +1,11 @@
 package com.bridgelabz;
 
+import java.util.Comparator;
+
 public class Maxfinder {
-    public Integer maxNumberfinder(Integer num1, Integer num2, Integer num3) {
-        Integer max = num1;
+    
+    public <E extends Comparable> E maxValuefinder(E num1, E num2, E num3) {
+        E max = num1;
         if(max.compareTo(num2) < 0)
             max = num2;
         if(max.compareTo(num3) < 0)
@@ -10,21 +13,5 @@ public class Maxfinder {
         return  max;
     }
 
-    public Float maxFloatfinder(Float num1, Float num2, Float num3) {
-        Float max = num1;
-        if(max.compareTo(num2) < 0)
-            max = num2;
-        if(max.compareTo(num3) < 0)
-            max = num3;
-        return  max;
-    }
 
-    public String maxStringfinder(String str1, String str2, String str3) {
-        String max = str1;
-        if(max.compareTo(str2) < 0)
-            max = str2;
-        if(max.compareTo(str3) < 0)
-            max = str3;
-        return  max;
-    }
 }
--- src/test/java/com/bridgelabz/TestMax.java
+++ src/test/java/com/bridgelabz/TestMax.java
@@ -10,63 +10,63 @@ public class TestMax {
     @Test
     public  void givenThreeinteger_whenMaxNumberAtFirstposition_shouldReturnFirstnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Integer result = maxfinder.maxNumberfinder(30, 20, 10);
+        Integer result = maxfinder.maxValuefinder(30, 20, 10);
         Assert.assertEquals(Integer.valueOf(30), result);
     }
 
     @Test
     public  void givenThreeinteger_whenMaxNumberAtSecondposition_shouldReturnSecondnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Integer result = maxfinder.maxNumberfinder(20, 30, 10);
+        Integer result = maxfinder.maxValuefinder(20, 30, 10);
         Assert.assertEquals(Integer.valueOf(30), result);
     }
 
     @Test
     public  void givenThreeinteger_whenMaxNumberAtThirdposition_shouldReturnThirdnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Integer result = maxfinder.maxNumberfinder(20, 10, 30);
+        Integer result = maxfinder.maxValuefinder(20, 10, 30);
         Assert.assertEquals(Integer.valueOf(30), result);
     }
 
     @Test
     public  void givenThreefloatVal_whenMaxNumberAtFirstposition_shouldReturnFirstnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Float result = maxfinder.maxFloatfinder(30.0f, 10.0f, 20.0f);
+        Float result = maxfinder.maxValuefinder(30.0f, 10.0f, 20.0f);
         Assert.assertEquals(Float.valueOf(30.0f), result);
     }
 
     @Test
     public  void givenThreefloatVal_whenMaxNumberAtSecondposition_shouldReturnSecondnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Float result = maxfinder.maxFloatfinder(20.0f, 30.0f, 20.0f);
+        Float result = maxfinder.maxValuefinder(20.0f, 30.0f, 20.0f);
         Assert.assertEquals(Float.valueOf(30.0f), result);
     }
 
     @Test
     public  void givenThreefloatVal_whenMaxNumberAtThirdposition_shouldReturnThirdnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        Float result = maxfinder.maxFloatfinder(20.0f, 10.0f, 30.0f);
+        Float result = maxfinder.maxValuefinder(20.0f, 10.0f, 30.0f);
         Assert.assertEquals(Float.valueOf(30.0f), result);
     }
 
     @Test
     public  void givenThreestringVal_whenMaxStringAtFirstposition_shouldReturnFirstnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        String result = maxfinder.maxStringfinder("Peach", "Banana", "Apple");
+        String result = maxfinder.maxValuefinder("Peach", "Banana", "Apple");
         Assert.assertEquals(String.valueOf("Peach"), result);
     }
 
     @Test
     public  void givenThreestringVal_whenMaxStringAtSecondposition_shouldReturnSecondnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        String result = maxfinder.maxStringfinder("Banana", "Peach", "Apple");
+        String result = maxfinder.maxValuefinder("Banana", "Peach", "Apple");
         Assert.assertEquals(String.valueOf("Peach"), result);
     }
 
     @Test
     public  void givenThreestringVal_whenMaxStringAtThirdposition_shouldReturnThirdnumber() {
         Maxfinder maxfinder = new Maxfinder();
-        String result = maxfinder.maxStringfinder("Apple", "Banana", "Peach");
+        String result = maxfinder.maxValuefinder("Apple", "Banana", "Peach");
         Assert.assertEquals(String.valueOf("Peach"), result);
     }
 }

b07c6c7a9a32397d6a110cd15cae6f50f103b3be
aishwaryap14 <aishwarya.potdar123@gamil.com>
2021-04-07 20:14:23 +0530

    Added: functionality to print max value

--- src/main/java/com/bridgelabz/Maxfinder.java
+++ src/main/java/com/bridgelabz/Maxfinder.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Maxfinder {
-    
-    public <E extends Comparable> E maxValuefinder(E... values) {
-        return Arrays.stream(values).max((i, j)-> i.compareTo(j)).get();
 
+    
+    public <E extends Comparable> E maxValuefinder(E... values)
+    {
+        E max = Arrays.stream(values).max((i, j)-> i.compareTo(j)).get();
+        toPrintMaxvalue(max);
+        return max;
+    }
+
+    public <E extends Comparable> void toPrintMaxvalue(E max)
+    {
+        System.out.println(max);
     }
 
 
